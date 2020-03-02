70fe37be99e619c72b4e0e6090848198df03bcac
parvathysjsu <parvathy.kannankumarathmadomkrishnan@sjsu.edu>
2020-03-01 18:49:04 -0800

    submit event

--- src/Form.js
+++ src/Form.js
@@ -3,6 +3,20 @@ import './App.css';
 import InputBox from './InputBox';
 
 export default class Form extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: "Please fill the form",
+    };
+  }
+
+  handleClick() {
+    let squares = this.state.status.slice();
+    squares = "Thank you for filling the form!!";
+    this.setState({status: squares});
+  }
+
   render() {
     return (
         <div class="container">
@@ -19,8 +33,9 @@ export default class Form extends Component {
           name="Subject"
           id="subject"
         />
-       <button type="button" class = "buttonSubmit"> Submit </button>
+       <button type="button" class = "buttonSubmit" onClick={() => this.handleClick()}> Submit </button>      
       </form>
+      <h3> {this.state.status}</h3>
       </div>
     );
   }
